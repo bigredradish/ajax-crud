fd8b86c2160f4a35ae138398a642d7e05b5eeee1
David Radford <dave@bigredradish.com>
2018-02-09 14:53:33 +0000

    Update create-product.js

--- app/products/create-product.js
+++ app/products/create-product.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function(){
+ 
+    // show html form when 'create product' button was clicked
+    $(document).on('click', '.create-product-button', function(){
+        // categories api call will be here
+    });
+ 
+    // 'create product form' handle will be here
+});
